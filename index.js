61153e991c07d14071f4319f3e0159b3d496db63
ta-chibana <tachibana.gmc@gmail.com>
2018-06-30 17:38:14 +0900

    Add waitUntil

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('http://bento-shogun.jp/menu/week/');
-  await page.screenshot({ path: 'example.png' });
+  await page.goto('http://bento-shogun.jp/menu/week/', {
+    waitUntil: 'networkidle0'
+  });
+  await page.screenshot({ path: 'example.png', fullPage: true });
 
   await browser.close();
 })();
