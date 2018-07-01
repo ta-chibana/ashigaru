9782a76d4460cb61c3bb5b52215e4e620c43408e
ta-chibana <tachibana.gmc@gmail.com>
2018-07-01 15:59:24 +0900

    Add function to get image urls

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const puppeteer = require('puppeteer');
 
+const BASE_URL = 'http://bento-shogun.jp/menu';
+// const TODAY_MENU_URL = `${BASE_URL}/today/`;
+const WEEK_MENU_URL = `${BASE_URL}/week/`;
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('http://bento-shogun.jp/menu/week/', {
+
+  await page.goto(WEEK_MENU_URL, {
     waitUntil: 'networkidle0'
   });
-  await page.screenshot({ path: 'example.png', fullPage: true });
+
+  const imageUrls = await page.$$eval('.item-image', images => {
+    return Array.prototype.map.call(images, image => (
+      image
+        .getAttribute('style')
+        .match(/background-image:url\(([\w|:|\/\-|\.|\.\?]+)\);/)[1]
+    ));
+  });
+
+  console.log(imageUrls);
 
   await browser.close();
 })();
