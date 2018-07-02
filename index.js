48cd2d518ac80150e5f9fc45a5d134b37b452e07
ta-chibana <tachibana.gmc@gmail.com>
2018-07-03 02:05:41 +0900

    Modify to display the menu

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 #! /usr/bin/env /usr/local/bin/node
 const puppeteer = require('puppeteer');
 
-const WEEK_MENU_URL = 'http://bento-shogun.jp/menu/week/';
+const MENU_URL = 'http://bento-shogun.jp/menu/today/';
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
-  await page.goto(WEEK_MENU_URL, {
+  await page.goto(MENU_URL, {
     waitUntil: 'networkidle0'
   });
 
-  const imageUrls = await page.$$eval('.item-image', images => {
-    return Array.prototype.map.call(images, image => (
-      image
+  const items = await page.$$eval('.item', items => {
+    return Array.prototype.map.call(items, item => {
+      const name = item.querySelector('.item-name').innerText;
+
+      const imageUrl = item
+        .querySelector('.item-image')
         .getAttribute('style')
-        .match(/background-image:url\(([\w|:|\/\-|\.|\.\?]+)\);/)[1]
-    ));
+        .match(/background-image:url\(([\w|:|\/\-|\.|\.\?]+)\);/)[1];
+
+      return { name, imageUrl };
+    });
   });
 
   await browser.close();
 
   console.log('🍤');
   console.log('---');
-  for (let url of imageUrls) {
-    console.log(url);
+  for (let item of items) {
+    const encodedUrl = Buffer
+      .from(item.imageUrl, 'utf8')
+      .toString('base64');
+    console.log(`${item.name}|image=${encodedUrl}`);
+    console.log('---');
   }
 })();
